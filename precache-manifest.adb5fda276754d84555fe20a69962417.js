48f32c8a281228a19a3180bdd78193ebadb8caea
Shevchenko Elena <elena.kadru@gmail.com>
2020-09-06 23:46:02 +0300

    Updates

---
+++ precache-manifest.adb5fda276754d84555fe20a69962417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf6f39f83b76ebfc06eedf4dce2cf054",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "1b2d6afd4b7ae37226ca",
+    "url": "/goit-react-hw-02-phonebook/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "1b2d6afd4b7ae37226ca",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.43a46e32.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.43a46e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fea3beb07b95a858f089",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.58ea841c.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
